5348b4658f985e9d6a7b37feb7c378417676d650
Janaka Jayasuriya <39936547+janakaaleph@users.noreply.github.com>
2020-09-30 18:26:16 +0800

    Update generate-punch-card.sh

--- generate-punch-card.sh
+++ generate-punch-card.sh
@@ -1,6 +1,64 @@
 #!/bin/sh
 
-git punchcard ~/AlephProjects/Savvy/*/.git -o "Savvy-21-09-2020-27-09-2020.png" --title "Savvy - 21/09/2020 - 27/09/2020" --since 21.09.2020 --until=27.09.2020 --timezone UTC+08:00
-git punchcard ~/AlephProjects/Savvy/*/.git -o "Savvy-14-09-2020-20-09-2020.png" --title "Savvy - 14/09/2020 - 20/09/2020" --since 14.09.2020 --until=20.09.2020 --timezone UTC+08:00
-git punchcard ~/AlephProjects/Savvy/*/.git -o "Savvy-07-09-2020-13-09-2020.png" --title "Savvy - 07/09/2020 - 13/09/2020" --since 07.08.2020 --until=13.09.2020 --timezone UTC+08:00
-git punchcard ~/AlephProjects/Savvy/*/.git -o "Savvy-31-08-2020-06-09-2020.png" --title "Savvy - 31/08/2020 - 06/09/2020" --since 31.08.2020 --until=06.09.2020 --timezone UTC+08:00
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -d|--directory)
+    DIRECTORY="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -p|--prefix)
+    PREFIX="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -s|--since)
+    SINCE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -u|--until)
+    UNTIL="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -t|--timezone)
+    TIMEZONE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+SLASH=/
+HYPHEN=-
+SINCE_SLASH=${SINCE//./$SLASH}
+SINCE_HYPHEN=${SINCE//./$HYPHEN}
+UNTIL_SLASH=${UNTIL//./$SLASH}
+UNTIL_HYPHEN=${UNTIL//./$HYPHEN}
+
+# Used for debugging
+# echo "DIRECTORY     = ${DIRECTORY}"       # ~/AlephProjects/Savvy/*/.git
+# echo "PREFIX        = ${PREFIX}"          # Savvy
+# echo "SINCE         = ${SINCE}"           # 21.09.2020
+# echo "SINCE_SLASH   = ${SINCE_SLASH}"
+# echo "SINCE_HYPHEN  = ${SINCE_HYPHEN}"
+# echo "UNTIL         = ${UNTIL}"           # 21.09.2020
+# echo "UNTIL_SLASH   = ${UNTIL_SLASH}"
+# echo "UNTIL_HYPHEN  = ${UNTIL_HYPHEN}"
+# echo "TIMEZONE      = ${TIMEZONE}"        # UTC+08:00
+
+git punchcard $DIRECTORY -o "$PREFIX-$SINCE_HYPHEN-$UNTIL_HYPHEN.png" --title "$PREFIX - $SINCE_SLASH - $UNTIL_SLASH" --since $SINCE --until=$UNTIL --timezone $TIMEZONE
