2d38f0333ac718d1c90a28c80e199723dfcc6d18
Janaka Jayasuriya <39936547+janakaaleph@users.noreply.github.com>
2020-09-30 17:59:20 +0800

    Create generate-punch-card.sh

---
+++ generate-punch-card.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+git punchcard ~/AlephProjects/Savvy/*/.git -o "Savvy-21-09-2020-27-09-2020.png" --title "Savvy - 21/09/2020 - 27/09/2020" --since 21.09.2020 --until=27.09.2020 --timezone UTC+08:00
+git punchcard ~/AlephProjects/Savvy/*/.git -o "Savvy-14-09-2020-20-09-2020.png" --title "Savvy - 14/09/2020 - 20/09/2020" --since 14.09.2020 --until=20.09.2020 --timezone UTC+08:00
+git punchcard ~/AlephProjects/Savvy/*/.git -o "Savvy-07-09-2020-13-09-2020.png" --title "Savvy - 07/09/2020 - 13/09/2020" --since 07.08.2020 --until=13.09.2020 --timezone UTC+08:00
+git punchcard ~/AlephProjects/Savvy/*/.git -o "Savvy-31-08-2020-06-09-2020.png" --title "Savvy - 31/08/2020 - 06/09/2020" --since 31.08.2020 --until=06.09.2020 --timezone UTC+08:00
